71ea5706bb93671662b88fded5960e23f3398a37
cometang <1403029829@qq.com>
2019-11-19 22:03:10 +0800

    back第一次

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require('http')
+let users = [
+    { id: 1, name: '啦啦啦' },
+    { id: 2, name: '撒旦撒旦' }
+]
+let server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    if (req.url === '/api/users') {
+        res.end(JSON.stringify(users))
+    }else{
+        res.end('NOT Found')
+    }
+})
+
+server.listen(3000,()=>{
+    console.log('客户端服务器已经启动了，端口3000')
+})
